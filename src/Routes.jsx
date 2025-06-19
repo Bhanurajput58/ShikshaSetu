38b88e278230666d91cb162bbfecbc0b4b2c8400
Bhanurajput58 <www.bhanurajput5965@gmail.com>
2025-06-19 23:59:48 +0530

    main entry and routing setup

---
+++ src/Routes.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Courses from './pages/Courses';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
